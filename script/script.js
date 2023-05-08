fe496598d83717d867dd84f87a17a4e9ad2f6a18
kmontgomery <kimbrleemnt@gmail.com>
2023-05-07 23:31:44 -0400

    media queries added

---
+++ script/script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("hamButton");
+const nav = document.getElementById("mainMenu");
+
+button.addEventListener('click', () => {
+  nav.classList.toggle('show');
+
+
+});
